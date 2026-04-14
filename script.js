1d988d899c41fc60c6527871782b97edcdaf5a95
ssoumyaomm <ssoumya.omm@gmail.com>
2026-04-14 19:24:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
-// Initialize map
-const map = L.map('map').setView([20, 0], 2);
+var map = L.map('map').setView([20, 0], 2);
 
-// Map tiles
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 5,
 }).addTo(map);
 
-// Load country data
-fetch('data.json')
+// Load GeoJSON (for country shapes)
+fetch('https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json')
   .then(res => res.json())
-  .then(data => {
+  .then(geoData => {
 
-    data.forEach(country => {
+    // Load your custom data.json
+    fetch('data.json')
+      .then(res => res.json())
+      .then(countryData => {
 
-      const marker = L.marker([country.lat, country.lng]).addTo(map);
+        L.geoJSON(geoData, {
+          onEachFeature: function (feature, layer) {
 
-      marker.on('click', () => {
-        document.getElementById('details').innerHTML = `
-          <strong>${country.name}</strong><br/>
-          Alliances: ${country.alliances.join(", ")}<br/>
-          Conflicts: ${country.conflicts.join(", ")}<br/>
-          Trade: ${country.trade.join(", ")}
-        `;
+            layer.on('click', function () {
+              let name = feature.properties.name;
+
+              // Find matching country in your JSON
+              let data = countryData.find(c => c.name === name);
+
+              if (data) {
+                document.getElementById("details").innerText =
+                  `Country: ${name}
+Alliances: ${data.alliances.join(", ")}
+Conflicts: ${data.conflicts.join(", ")}
+Trade: ${data.trade.join(", ")}`;
+              } else {
+                document.getElementById("details").innerText =
+                  `Country: ${name} (No data available)`;
+              }
+            });
+
+          }
+        }).addTo(map);
+
       });
-
-    });
 
   });
